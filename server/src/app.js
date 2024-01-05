2b803bb3187a913fadcb16c361e049552890901d
PranavJaju <100665512+PranavJaju@users.noreply.github.com>
2024-01-05 18:22:09 +0000

    env

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const dotenv = require("dotenv")
-dotenv.config({path:"../../.env"})
+dotenv.config()
 const cookieParser = require('cookie-parser')
 const cors = require("cors")
 const port = process.env.PORT || 5000;
